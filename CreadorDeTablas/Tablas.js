027bb982a01ca22ccc5c633862a2ce723d8a1b82
ChrisMichiNotDev <chrismichidominacionmundial@gmail.com>
2025-08-29 16:14:02 -0700

    Arreglado el reinicio del CSS personalizado cuando está vacío Tablas.js

--- CreadorDeTablas/Tablas.js
+++ CreadorDeTablas/Tablas.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
 // Page has loaded
 var storageCSS = localStorage.getItem('CSSCustom');
 document.querySelector("#textoarray").placeholder = document.querySelector("#textoarray").placeholder.replaceAll('\\n', '\n')
- if (storageCSS != '' || storageCSS !== null) {
+ if (storageCSS != '' && storageCSS !== null) {
   document.getElementById('CustomCSS').value = storageCSS
  }
 eventlisteners();
@@ -681,3 +681,4 @@ const imgcla = "M12 8a4 4 0 1 1-8 0 4 4 0 0 1 8 0zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 
 const imgdrk = "M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z";
 
 listaceldas = [];
+
